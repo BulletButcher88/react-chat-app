2551c461ed208e73cc3d8ebba90fde139090db02
Bull-Butcher <markjamesbutcher@gmail.com>
2019-08-13 16:49:44 +1000

    event.js rallback to orginal format, error to emit on creating new user still accurs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import Layout from './components/Layout.js';
+import React, { Component } from 'react';
+import Layout from './components/Layout';
 import './index.css';
 
-function App() {
-  return (
-       <>
-       
-      <Layout title="Chat App"/>
-      </>
-  );
+class App extends Component {
+  
+  render() {
+    return (
+         <>
+        <Layout />
+        </>
+    );
+  }
 }
 
 export default App;
